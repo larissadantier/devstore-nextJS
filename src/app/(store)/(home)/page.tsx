67a6b4359f95cbf74d1fd64bf1082e41b0aab479
larissadantier <larissa_dantier@hotmail.com>
2024-05-14 22:38:03 -0300

    feat: first estructure project

---
+++ src/app/(store)/(home)/page.tsx
@@ -0,0 +1,74 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <div className="grid-rows-6 grid max-h-[860px] grid-cols-9 gap-6">
+      <Link
+        href="/"
+        className="group relative col-span-6 row-span-6 flex items-start justify-center overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          className="transition-transform duration-500 group-hover:scale-105"
+          src="/moletom-never-stop-learning.png"
+          width={920}
+          height={920}
+          quality={100}
+          alt=""
+        />
+
+        <div className="absolute bottom-28 right-28 flex h-12 max-w-[240px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Moletom Never S...</span>
+
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 flex items-center justify-center overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          className="transition-transform duration-500 group-hover:scale-105"
+          src="/moletom-ai-side.png"
+          width={345}
+          height={345}
+          quality={100}
+          alt=""
+        />
+
+        <div className="absolute bottom-10 right-10 flex h-12 max-w-[240px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Moletom Never S...</span>
+
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
+
+      <Link
+        href="/"
+        className="group relative col-span-3 row-span-3 flex items-center justify-center overflow-hidden rounded-lg bg-zinc-900"
+      >
+        <Image
+          className="transition-transform duration-500 group-hover:scale-105"
+          src="/camiseta-dowhile-2022.png"
+          width={345}
+          height={345}
+          quality={100}
+          alt=""
+        />
+
+        <div className="absolute bottom-10 right-10 flex h-12 max-w-[240px] items-center gap-2 rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
+          <span className="truncate text-sm">Moletom Never S...</span>
+
+          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
+            R$129
+          </span>
+        </div>
+      </Link>
+    </div>
+  )
+}
